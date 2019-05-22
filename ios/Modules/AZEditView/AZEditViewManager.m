33acc64ba09facd291e6c85c4fbad10faa4a07d9
高昇 <xiaogao@gaoshengdeMacBook-Pro.local>
2019-05-22 14:04:20 +0800

    添加cocoapods管理，桥接react-native

---
+++ ios/Modules/AZEditView/AZEditViewManager.m
@@ -0,0 +1,20 @@
+//
+//  AZEditViewManager.m
+//  ReactNativeAztecDemo
+//
+//  Created by 高昇 on 2019/5/22.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "AZEditViewManager.h"
+
+@implementation AZEditViewManager
+
+RCT_EXPORT_MODULE()
+
+- (UIView *)view
+{
+  return [[UIView alloc] init];
+}
+
+@end
---
+++ ios/Pods/Target Support Files/Pods-ReactNativeAztecDemo/Pods-ReactNativeAztecDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeAztecDemo : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeAztecDemo
+@end
